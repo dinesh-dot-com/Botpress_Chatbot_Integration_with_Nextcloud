6be322baab102616c80f74d5915b575a21a62033
Dinesh <105905278+DineshRefex@users.noreply.github.com>
2023-01-09 11:45:48 +0530

    Create config.php

---
+++ var/www/html/nextcloud/config/config.php
@@ -0,0 +1,49 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocqkjfgd4b63',
+  'passwordsalt' => 's2g1BqrR3dENC+ZAS/uIfCGhB1Zp9C',
+  'secret' => 'SWK97duMOfN15Qenq8TM/GqGs3ngp/MUEr1OCSjp5Y+E0NuC',
+  'forcessl' => true,
+  'trashbin_retention_obligation' => 'auto,90',
+  'forceSSLforSubdomains' => true,
+  'trusted_proxies' => 
+  array (
+    0 => 'https://example.com/',
+    1 => 'https://nextcloud/apps/dashboard/templates/index.html',
+    2 => 'exampe1.com',
+    3 => 'https://www.insegment.com/blog/wp-content/uploads/2020/11/chatbot-marketing.gif',
+  ),
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.**',
+    1 => '**.98.**.**',
+    2 => 'https://example/',
+    3 => 'nextcloud.com',
+    4 => 'https://example1/apps/dashboard/templates/index.html',
+    5 => 'https://www.insegment.com/blog/wp-content/uploads/2020/11/chatbot-marketing.gif',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '25.0.2.3',
+  'overwrite.cli.url' => 'https://example1.refex.group/apps/dashboard/templates/index.html',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+  'app_install_overwrite' => 
+  array (
+    0 => 'ocsms',
+    1 => 'jitsi',
+    2 => 'hidesidebars',
+    3 => 'impersonate',
+    4 => 'nxt',
+    5 => 'talked',
+  ),
+  'theme' => '',
+  'loglevel' => 0,
+  'maintenance' => false,
+);
