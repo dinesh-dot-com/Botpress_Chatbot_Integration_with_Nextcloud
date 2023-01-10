c8381f787b4084e9b4189b18266cc20f93a862ed
Dinesh <105905278+DineshRefex@users.noreply.github.com>
2023-01-10 12:32:14 +0530

    Update layout.user.php

--- var/www/html/nextcloud/core/templates/layout.user.php
+++ var/www/html/nextcloud/core/templates/layout.user.php
@@ -24,10 +24,6 @@ $getUserAvatar = static function (int $size) use ($_): string {
 			?>
 		</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-		<?php if ($theme->getiTunesAppId() !== '') { ?>
-		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
-		<?php } ?>
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<meta name="apple-mobile-web-app-title" content="<?php p((!empty($_['application']) && $_['appid'] != 'files')? $_['application']:$theme->getTitle()); ?>">
